b11d781db6d94c6ff7efa6601c836287e04274c6
Roy Musthang <darksiders20005@gmail.com>
2021-05-25 15:12:45 -0300

    desafio2 concluido

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,9 @@ function compareTrue(valor1, valor2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let triangulo = base * height / 2
+  return triangulo
 }
 
 // Desafio 3
